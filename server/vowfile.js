36a748a297da73ea62f3d325512e6f9d5b30716d
Arcoders <anonismaa@gmail.com>
2018-08-08 16:25:39 +0200

    run migration before test, and reset it after test?

--- server/vowfile.js
+++ server/vowfile.js
@@ -11,7 +11,7 @@
 */
 
 // Uncomment when want to run migrations
-// const ace = require('@adonisjs/ace')
+const ace = require('@adonisjs/ace')
 
 module.exports = (cli, runner) => {
   runner.before(async () => {
@@ -34,7 +34,7 @@ module.exports = (cli, runner) => {
     | Migrate the database before starting the tests.
     |
     */
-    // await ace.call('migration:run')
+    await ace.call('migration:run')
   })
 
   runner.after(async () => {
@@ -57,6 +57,6 @@ module.exports = (cli, runner) => {
     | original state
     |
     */
-    // await ace.call('migration:reset')
+    await ace.call('migration:reset')
   })
 }
